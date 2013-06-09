eb222867c1e78a2f6fcac73db93780212a05b1d9
ntakeda67 <n.takeda67@gmail.com>
2013-06-09 16:01:06 +0900

    DataPointsによるTheorieテスト実装

--- prac1801/src/main/java/org/nt67/junitprac/prac1801/util/StringUtils.java
+++ prac1801/src/main/java/org/nt67/junitprac/prac1801/util/StringUtils.java
@@ -1,8 +1,28 @@
 package org.nt67.junitprac.prac1801.util;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringUtils {
+	private static String SNAKE_CASE_DELIMITER = "_";
+
 	public static String toSnakeCase(String target){
-		return null;
+		if(target == null){
+			return null;
+		}
+
+		String snake = target;
+		Pattern p = Pattern.compile("([A-Z])");
+		for(;;){
+			Matcher m = p.matcher(snake);
+			if(!m.find()) {
+				break;
+			}
+			snake = m.replaceFirst(SNAKE_CASE_DELIMITER + m.group(1).toLowerCase());
+			System.out.println(m.group(1));
+		}
+
+		return snake.replaceFirst("^_", "");
 	}
 
 }
--- prac1801/src/test/java/org/nt67/junitprac/prac1801/util/StringUtilsTest.java
+++ prac1801/src/test/java/org/nt67/junitprac/prac1801/util/StringUtilsTest.java
@@ -1,14 +1,40 @@
 package org.nt67.junitprac.prac1801.util;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
 
+@RunWith(Theories.class)
 public class StringUtilsTest {
+	@DataPoints
+	public static ToSnakeCaseFixture[] TO_SNAKE_CASE_FIXTUREs = {
+		//new ToSnakeCaseFixture("a", "a"),
+		new ToSnakeCaseFixture("aaa", "aaa"),
+		new ToSnakeCaseFixture("abc", "abc"),
+		new ToSnakeCaseFixture("a1c", "a1c"),
+		new ToSnakeCaseFixture("testMethod", "test_method"),
+		new ToSnakeCaseFixture(null, null),
+		new ToSnakeCaseFixture("ManyManyManyManyWords", "many_many_many_many_words")
+	};
 
-	@Test
-	public void toSnakeCase_oneWord() {
-		fail("Not yet implemented");
+	@Theory
+	public void toSnakeCaseTest(ToSnakeCaseFixture fixture) {
+		assertThat(StringUtils.toSnakeCase(fixture.arg), is(fixture.expected));
+	}
+
+	static class ToSnakeCaseFixture {
+		String arg;
+		String expected;
+		ToSnakeCaseFixture(String arg, String expected){
+			this.arg = arg;
+			this.expected = expected;
+		}
 	}
 
 }
